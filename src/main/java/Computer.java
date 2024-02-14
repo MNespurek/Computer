97c5e82d0141fcbde84a8467555b4ca512a3e5ab
michal.nespurek <michal.nespurek@email.cz>
2024-02-14 13:03:14 +0100

    Třída Počítač
    vytvoření třídy počítač s atributy a dvěma konstruktory

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Computer {
+import java.math.BigDecimal;
+
+public class Computer {
+    private String description;
+    private int yearOfProduction;
+    private BigDecimal price;
+
+    private Computer(String description, int yearOfProduction, BigDecimal price) {
+        this.description = description;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+    }
+
+    private Computer(String description) {
+        this(description, 1800, new BigDecimal(2000));
+    }
 }
