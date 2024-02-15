22992bff2bba3486b489979e54027b2059dc41d6
michal.nespurek <michal.nespurek@email.cz>
2024-02-15 12:43:21 +0100

    Třídní řazení seznamu
    zahrnuta varianta řazení třídního se zapouzdřeným seznamem a členěním dle třídy ComputerComparator

--- src/main/java/com/engeto/pruduct/Main.java
+++ src/main/java/com/engeto/pruduct/Main.java
@@ -10,14 +10,20 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Computer computer1 = new Computer("Dell", 1980, BigDecimal.valueOf(20000));
+        Computer computer1 = new Computer("Zell", 1980, BigDecimal.valueOf(20000));
         Computer computer2 = new Computer("Dell", 2010, BigDecimal.valueOf(21000));
         Computer computer3 = new Computer("Mac", 2009, BigDecimal.valueOf(15000));
+        Computer computer4 = new Computer("Mac", 2009, BigDecimal.valueOf(18000));
+        Computer computer5 = new Computer("Mac", 2009, BigDecimal.valueOf(17000));
+
+
         List<Computer> otherList = new ArrayList<>();
         ComputerManager manager = new ComputerManager(otherList);
         manager.addComputer(computer1);
         manager.addComputer(computer2);
         manager.addComputer(computer3);
+        manager.addComputer(computer4);
+        manager.addComputer(computer5);
         System.out.println(manager.getComputers().size());
         for (Computer computer : manager.getComputers()) {
             System.out.println("Název PC: " +computer.getDescription()+ " rok: " +computer.getYearOfProduction()+ " cena: " +computer.getPrice()+ " Kč");
@@ -33,6 +39,15 @@ public class Main {
         System.out.println("Řada computer reverzně dle datu výroby včetně nového listu: \n");
         priceSortedList.sort(Comparator.comparing(Computer::getYearOfProduction).reversed());
         priceSortedList.forEach(System.out::println);
+        List<Computer> classSortedList = new ArrayList<>(manager.getComputers());
+        Collections.sort(classSortedList);
+        System.out.println("Řada computer reverzně dle datu výroby včetně nového listu a třídního členění: \n");
+        classSortedList.forEach(System.out::println);
+        Collections.sort(classSortedList, new ComputerComparator());
+        System.out.println("Řada computer reverzně dle datu výroby včetně nového listu a členění dle třídy ComputerComparator: \n");
+        classSortedList.forEach(System.out::println);
+
+
 
     }
 
